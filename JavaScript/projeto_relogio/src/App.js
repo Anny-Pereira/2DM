b2cbd3c6640f0f595da6ca230a44fc73747b1d06
Anny Pereira <isabelle.pereira04@gmail.com>
2021-10-19 23:47:12 -0300

    Exemplo Relogio

--- JavaScript/projeto_relogio/src/App.js
+++ JavaScript/projeto_relogio/src/App.js
@@ -7,6 +7,9 @@ function DataFormatada(props) {
   return <h2>Horário Atual: {props.date.toLocaleTimeString()}</h2>
 }
 
+
+
+
 class Relogio extends React.Component {
   constructor(props) {
     super(props);
@@ -24,13 +27,17 @@ class Relogio extends React.Component {
   //Ciclo de vida que ocorre quando Clock é inserido na DOM (nascimento)
   componentDidMount()
   {
-    setInterval(() => { this.tick()}, 1000);
+    this.timerID =  setInterval(() => { this.tick()}, 1000);
+
+    //Exibe no console o id de cada relógio
+    console.log("Eu sou o relógio " + this.timerID);
   }
 
 
   //Ciclo de vida que ocorre quando clock é removido da DOM (morte)
+  // Quando isso acontece, a função clearInterval() limpa o relógio criado pela setInterval
   componentWillUnmount(){
-
+    clearInterval(this.timerID);
   }
 
 
@@ -40,23 +47,37 @@ class Relogio extends React.Component {
     })
   }
 
+  pararRelogio()
+  {
+    clearInterval(this.timerID);
+    console.log("Relógio " + this.timerID + " pausado.")
+  }
+
+  retomarRelogio()
+  {
+    this.timerID =  setInterval(() => { this.tick()}, 1000);
+    console.log("Relógio retomado.");
+    console.log("Agora eu sou o relógio " + this.timerID);
+  }
+
   render() {
     return (
       <div>
         <h1>Quer Horas?</h1>
         <DataFormatada date={this.state.date} />
+        <div>
+          <button className="btn pausar" onClick={() => {this.pararRelogio()}}>Pausar</button>
+          <button className="btn retomar" onClick={() => {this.retomarRelogio()}}>Retomar</button>
+        </div>
       </div>
     )
   }
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
 
 
 
@@ -72,4 +93,6 @@ function App() {
   );
 }
 
+
+//Declara que o componente App pode ser utilizado fora deste escopo
 export default App;
